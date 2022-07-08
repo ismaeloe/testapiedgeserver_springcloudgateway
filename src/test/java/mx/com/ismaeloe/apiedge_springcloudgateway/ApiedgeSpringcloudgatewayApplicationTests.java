5cef2e2ea2181c96a96c06229ca1f1869616e819
ismaeloe <ismaeloe@yahoo.com.mx>
2022-07-08 16:44:06 -0500

    first commit

---
+++ src/test/java/mx/com/ismaeloe/apiedge_springcloudgateway/ApiedgeSpringcloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.ismaeloe.apiedge_springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiedgeSpringcloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
