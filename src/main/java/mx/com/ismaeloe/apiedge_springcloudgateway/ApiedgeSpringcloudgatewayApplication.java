ce29738fd3f6fb3cae2fad8986109a35b07cf7f6
ismaeloe <ismaeloe@yahoo.com.mx>
2022-07-12 01:33:03 -0500

    Config with Java @Bean public RouteLocator

--- src/main/java/mx/com/ismaeloe/apiedge_springcloudgateway/ApiedgeSpringcloudgatewayApplication.java
+++ src/main/java/mx/com/ismaeloe/apiedge_springcloudgateway/ApiedgeSpringcloudgatewayApplication.java
@@ -2,7 +2,12 @@ package mx.com.ismaeloe.apiedge_springcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -12,4 +17,20 @@ public class ApiedgeSpringcloudgatewayApplication {
 		SpringApplication.run(ApiedgeSpringcloudgatewayApplication.class, args);
 	}
 
+	//ApplicationListener<ApplicationEvent>
+	
+	@Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+            // Add a simple re-route from: /get to: http://httpbin.org:80
+            // Add a simple "Hello:World" HTTP Header
+            .route(p -> p.path("/get") // intercept calls to the /get path
+            			.filters(f -> f.addRequestHeader("Hello", "World")) // add header
+            			.uri("http://httpbin.org:80")) // forward to httpbin
+            	//.uri("http://www.lapi.com.mx"))
+            .route( p -> p.path("/guides").uri("https://spring.io") ) //lb:customers/ =LoadBalancer from Custormer Services without DNS
+            .route( p -> p.path("/lapi").and().method( HttpMethod.GET).uri("rewrite.backend.uri:https://lapi.com.mx/") )
+            .build();
+    }
+	
 }
